c8128f588ea95fb2437426a161e130cf08c77554
Shane Dinh <shane.dinh5@gmail.com>
2022-06-30 18:18:17 -0500

    validate answers

--- app.js
+++ app.js
@@ -13,20 +13,41 @@ console.log(inquirer);
 
 const promptUser = () => {
     return inquirer.prompt([
-      {
+    {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
-      },
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+            return true;
+            } else {
+            console.log('Please enter your name!');
+            return false;
+            }
+        }
+        },
       {
         type: 'input',
         name: 'github',
         message: 'Enter your GitHub Username'
       },
       {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
+      },
+      {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+          if (confirmAbout) {
+            return true;
+          } else {
+            return false;
+          }
+        }
       }
     ]);
   };
